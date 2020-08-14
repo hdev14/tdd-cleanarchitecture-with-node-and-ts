663a0209614e220e189e983a984505217d24fd41
HermersonDev <ne.hermerson@gmail.com>
2020-08-14 10:37:03 -0300

    test: create test to ensure SignupController's handle returns 400 if no email is provided

--- src/presentation/controllers/signup-controller.test.ts
+++ src/presentation/controllers/signup-controller.test.ts
@@ -15,4 +15,19 @@ describe('SignUp Controller', () => {
     expect(httpResponse.status_code).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing param: name'))
   })
+
+  it('should return 400 if no email is provided', () => {
+    const signupController = new SignupController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        password: 'any_password',
+        password_confirm: 'any_password'
+      }
+    }
+
+    const httpResponse = signupController.handle(httpRequest)
+    expect(httpResponse.status_code).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
+  })
 })
