bc2a06e063e29222955281343eb2b7946aaab010
HermersonDev <ne.hermerson@gmail.com>
2020-08-14 11:26:08 -0300

    refactor: move http types and create controller interface

--- src/presentation/controllers/SignupController.ts
+++ src/presentation/controllers/SignupController.ts
@@ -1,8 +1,9 @@
-import { HttpRequest, HttpResponse } from '../protocols/http'
+import { HttpRequest, HttpResponse } from '../protocols/types/http'
+import Controller from '../protocols/interfaces/Controller'
 import MissingParamError from '../errors/MissingParamError'
 import { badRequest } from '../helpers/http'
 
-export default class SignupController {
+export default class SignupController implements Controller {
   public handle (httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'email', 'password', 'password_confirm']
 
