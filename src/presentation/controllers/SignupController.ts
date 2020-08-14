6962fe9ee6fe84d2f2400419860759c1e809385b
HermersonDev <ne.hermerson@gmail.com>
2020-08-14 11:03:58 -0300

    refactor: create http bad request helper

--- src/presentation/controllers/SignupController.ts
+++ src/presentation/controllers/SignupController.ts
@@ -1,20 +1,15 @@
 import { HttpRequest, HttpResponse } from '../protocols/http'
 import MissingParamError from '../errors/MissingParamError'
+import { badRequest } from '../helpers/http'
 
 export default class SignupController {
   public handle (httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.name) {
-      return {
-        status_code: 400,
-        body: new MissingParamError('name')
-      }
+      return badRequest(new MissingParamError('name'))
     }
 
     if (!httpRequest.body.email) {
-      return {
-        status_code: 400,
-        body: new MissingParamError('email')
-      }
+      return badRequest(new MissingParamError('email'))
     }
   }
 }
