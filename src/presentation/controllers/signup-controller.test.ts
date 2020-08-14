86119429e05dbc0acd16d88b75c4146dd0260b8d
HermersonDev <ne.hermerson@gmail.com>
2020-08-14 11:16:02 -0300

    test: create test to ensure SignupController's handle returns 400 if no password_confirm is provided

--- src/presentation/controllers/signup-controller.test.ts
+++ src/presentation/controllers/signup-controller.test.ts
@@ -46,4 +46,19 @@ describe('SignUp Controller', () => {
     expect(httpResponse.status_code).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('password'))
   })
+
+  it('should return 400 if no password_confirm is provided', () => {
+    const signupController = new SignupController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any@mail.com',
+        password: 'any_password'
+      }
+    }
+
+    const httpResponse = signupController.handle(httpRequest)
+    expect(httpResponse.status_code).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('password_confirm'))
+  })
 })
