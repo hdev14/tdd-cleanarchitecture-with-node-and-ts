512e369a060f5468e806397c8ba795d0d0048158
HermersonDev <ne.hermerson@gmail.com>
2020-08-15 11:56:58 -0300

    refactor: change test titles

--- src/utils/email-validator-adapter.test.ts
+++ src/utils/email-validator-adapter.test.ts
@@ -8,14 +8,14 @@ jest.mock('validator', () => ({
 }))
 
 describe('EmailValidator Adapter', () => {
-  it('should return false if EmailValidatorAdapter.isValid return false', () => {
+  it('should return false if validator.isEmail return false', () => {
     const emailValidatorAdapter = new EmailValidatorAdapter()
     jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
     const emailIsValid = emailValidatorAdapter.isValid('invalid@mail.com')
     expect(emailIsValid).toBe(false)
   })
 
-  it('should return true if EmailValidatorAdapter.isValid return true', () => {
+  it('should return true if validator.isEmail return true', () => {
     const emailValidatorAdapter = new EmailValidatorAdapter()
     const emailIsValid = emailValidatorAdapter.isValid('valid@mail.com')
     expect(emailIsValid).toBe(true)
