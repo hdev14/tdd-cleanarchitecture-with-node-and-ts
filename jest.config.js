9a864d38dea3a8d78cc1b078555bc36d229567b7
HermersonDev <ne.hermerson@gmail.com>
2020-08-15 11:08:52 -0300

    chore: setup scripts to jest and husky

--- jest.config.js
+++ jest.config.js
@@ -18,14 +18,17 @@ module.exports = {
   // collectCoverage: false,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  collectCoverageFrom: ['<rrotDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/**/protocols/**/*.ts'
+  ],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
 
   // An array of regexp pattern strings used to skip coverage collection
   // coveragePathIgnorePatterns: [
-  //   "/node_modules/"
+  //   '/node_modules/',
   // ],
 
   // Indicates which provider should be used to instrument code for coverage
