1eb7351e277fc407a4464b67268c37f3772f2f49
HermersonDev <ne.hermerson@gmail.com>
2020-08-15 10:28:44 -0300

    style: change test title

--- src/presentation/controllers/signup-controller.test.ts
+++ src/presentation/controllers/signup-controller.test.ts
@@ -160,7 +160,7 @@ describe('SignUp Controller', () => {
     expect(httpResponse.body).toEqual(new ServerError())
   })
 
-  it('should call CreateAccount with correct data', () => {
+  it('should call CreateAccount.create with correct data', () => {
     const createSpy = jest.spyOn(createAccountMock, 'create')
 
     const httpRequest = {
