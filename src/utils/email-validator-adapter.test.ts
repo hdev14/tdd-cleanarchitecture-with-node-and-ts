3a8920d52d43caf750447a31921da6a93db53cdf
HermersonDev <ne.hermerson@gmail.com>
2020-08-15 12:02:53 -0300

    refactor: change instantiation of emailValidatorAdapter to beforeEach jest method

--- src/utils/email-validator-adapter.test.ts
+++ src/utils/email-validator-adapter.test.ts
@@ -8,21 +8,24 @@ jest.mock('validator', () => ({
 }))
 
 describe('EmailValidator Adapter', () => {
+  let emailValidatorAdapter: EmailValidatorAdapter
+
+  beforeEach(() => {
+    emailValidatorAdapter = new EmailValidatorAdapter()
+  })
+
   it('should return false if validator.isEmail return false', () => {
-    const emailValidatorAdapter = new EmailValidatorAdapter()
     jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
     const emailIsValid = emailValidatorAdapter.isValid('invalid@mail.com')
     expect(emailIsValid).toBe(false)
   })
 
   it('should return true if validator.isEmail return true', () => {
-    const emailValidatorAdapter = new EmailValidatorAdapter()
     const emailIsValid = emailValidatorAdapter.isValid('valid@mail.com')
     expect(emailIsValid).toBe(true)
   })
 
   it('should call validator.isEmail with correct email', () => {
-    const emailValidatorAdapter = new EmailValidatorAdapter()
     const isEmailSpy = jest.spyOn(validator, 'isEmail')
     emailValidatorAdapter.isValid('any@mail.com')
     expect(isEmailSpy).toHaveBeenCalledWith('any@mail.com')
