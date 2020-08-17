454c02fe353a15758b9b37bfa05d7b3d1442a69d
HermersonDev <ne.hermerson@gmail.com>
2020-08-17 10:32:43 -0300

    test: create test to ensure Encrypter.encrypt is called with correct value

---
+++ src/data/usecases/db-create-account.test.ts
@@ -0,0 +1,23 @@
+import DbCreateAccount from './DbCreateAccount'
+import IEncrypter from '../protocols/interfaces/IEncrypter'
+
+class EncrypterMock implements IEncrypter {
+  async encrypt (value: string): Promise<string> {
+    return Promise.resolve('hashed')
+  }
+}
+
+describe('DbCreateAccount UseCase', () => {
+  it('should call Encrypter with correct value', async () => {
+    const encrypterMock = new EncrypterMock()
+    const dbCreateAccount = new DbCreateAccount(encrypterMock)
+    const encryptSpy = jest.spyOn(encrypterMock, 'encrypt')
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid@mail.com',
+      password: 'valid_password'
+    }
+    await dbCreateAccount.create(accountData)
+    expect(encryptSpy).toHaveBeenCalledWith(accountData.password)
+  })
+})
