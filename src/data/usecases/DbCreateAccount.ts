1d01498f4547137c9ce871e194b34a2530e0819f
HermersonDev <ne.hermerson@gmail.com>
2020-08-17 10:36:19 -0300

    refactor(data-layer): make DbCreateAccount implement ICreateAccount

--- src/data/usecases/DbCreateAccount.ts
+++ src/data/usecases/DbCreateAccount.ts
@@ -1,7 +1,8 @@
+import ICreateAccount from '../../domain/protocols/interfaces/ICreateAccount'
 import { AccountData, AccountModel } from '../../domain/protocols/types/account'
 import IEncrypter from '../protocols/interfaces/IEncrypter'
 
-export default class DbCreateAccount {
+export default class DbCreateAccount implements ICreateAccount {
   constructor (private readonly encrypter: IEncrypter) {}
 
   async create (accountData: AccountData): Promise<AccountModel> {
