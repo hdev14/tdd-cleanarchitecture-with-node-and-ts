5a8fb1c5c5293ee7e9df618633ec7db438c64739
HermersonDev <ne.hermerson@gmail.com>
2020-08-18 10:50:08 -0300

    test(infra-layer): create test to ensure BCryptAdapter.encrypt returns a hashed value

--- src/infra/cryptography/bcrypt-adpater.test.ts
+++ src/infra/cryptography/bcrypt-adpater.test.ts
@@ -1,6 +1,11 @@
 import bcrypt from 'bcrypt'
 import BCryptAdapter from './BCryptAdapter'
 
+jest.mock('bcrypt', () => ({
+  hash: async (data: any, saltOrRounds: number): Promise<string> => {
+    return Promise.resolve('hashed_value')
+  }
+}))
 describe('Unit Tests for BCryptAdapter class', () => {
   it('should call bcrypt.hash with correct value', async () => {
     const salt = 12
@@ -8,5 +13,12 @@ describe('Unit Tests for BCryptAdapter class', () => {
     const hashSpy = jest.spyOn(bcrypt, 'hash')
     await bcryptAdapter.encrypt('any_value')
     expect(hashSpy).toHaveBeenCalledWith('any_value', 12)
+  })
+
+  it('should return a hash on success', async () => {
+    const salt = 12
+    const bcryptAdapter = new BCryptAdapter(salt)
+    const hashedValue = await bcryptAdapter.encrypt('any_value')
+    expect(hashedValue).toBe('hashed_value')
   })
 })
